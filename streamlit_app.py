3a5fc9feeed888c58ed904897bb2ed008f30f72c
pri-snow <127393281+pri-snow@users.noreply.github.com>
2023-03-09 13:58:40 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -56,10 +56,12 @@ if streamlit.button('Geat Fruit Load List'):
   my_data_rows = get_fruit_load_list()
   streamlit.dataframe(my_data_rows)
 
-  streamlit.stop()
+streamlit.stop()
+
+
 
 add_my_fruit = streamlit.text_input('What fruit would you like to add?','Kiwi')
 streamlit.write('Thanks for adding ', add_my_fruit)
-
-
 my_cur.execute("insert into fruit_load_list values ('from streamlit')")
+
+
